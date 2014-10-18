21a2f3b934c516a7c31d79031915bf93cbeb330a
vecliucadi <vecliuc.adrian@gmail.com>
2014-10-18 18:23:34 +0300

    Implemented ASCell.java and SCell.java

--- PCBE/TheGameOfLife/src/goodfellas/TheGameOfLife/Cells/ASCell.java
+++ PCBE/TheGameOfLife/src/goodfellas/TheGameOfLife/Cells/ASCell.java
@@ -1,5 +1,28 @@
 package goodfellas.TheGameOfLife.Cells;
 
 public class ASCell extends AbstractCell{
+	
+	public ASCell(int T_Full,int T_Starve){
+		super(T_Full,T_Starve);
+	}
+	
+	
+	public void reproduce(){
+		
+		ASCell asCell1=new ASCell(123,12);
+		
+		if(asCell1.isReadyForReproduction()){
+			(new Thread(new ASCell(123,12))).run();
+			(new Thread(new ASCell(123,12))).run();
+		}
+		
+	}
+
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- PCBE/TheGameOfLife/src/goodfellas/TheGameOfLife/Cells/SCell.java
+++ PCBE/TheGameOfLife/src/goodfellas/TheGameOfLife/Cells/SCell.java
@@ -1,5 +1,39 @@
 package goodfellas.TheGameOfLife.Cells;
 
-public class SCell extends AbstractCell {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class SCell extends AbstractCell{
+	
+	
+	public SCell(int T_Full,int T_Starve){
+		super(T_Full,T_Starve);
+	}
+	
+	
+	public void reproduce(){
+		
+		Random randomizer = new Random();
+		List<AbstractCell> list = new ArrayList<AbstractCell>();
+		  list.add(new ASCell(123,3));
+		  list.add(new SCell(123,3));
+
+		
+		SCell sCell1=new SCell(123,3);
+		
+
+		if(sCell1.isReadyForReproduction()){
+			sCell1.findMate();
+			(new Thread(list.get(randomizer.nextInt(list.size())))).run();
+		
+		}
+	}
+
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
